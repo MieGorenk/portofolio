fc03b1995514cea511e78c426e69d4f4167995e6
unknown <ezzaardiala@gmail.com>
2020-07-02 11:11:57 +0700

    Updates

---
+++ precache-manifest.5353fb26887c775da9ddf4146cf2d509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdef31e164aae77caa8474c6bed069c5",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "0017475b6fff2e66a357",
+    "url": "/portofolio/static/css/main.cab71535.chunk.css"
+  },
+  {
+    "revision": "0786b98abb716dd32c04",
+    "url": "/portofolio/static/js/2.5cd16f1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.5cd16f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0017475b6fff2e66a357",
+    "url": "/portofolio/static/js/main.7c1cc83f.chunk.js"
+  },
+  {
+    "revision": "a3a6c8a1f81f721d2b09",
+    "url": "/portofolio/static/js/runtime-main.cef59536.js"
+  }
+]);
